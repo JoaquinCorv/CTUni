45982362be1ebcf05c6d8633fa8b9f43248149d9
Joaquín Corvalán <137232156+JoaquinCorv@users.noreply.github.com>
2023-09-01 15:07:32 -0300

    cree getters y setters

--- CTUni/src/main/java/com/dojo/workspace/CTUni/models/Usuario.java
+++ CTUni/src/main/java/com/dojo/workspace/CTUni/models/Usuario.java
@@ -42,4 +42,52 @@ public class Usuario {
 	@Size(min=8, message= "Password debe contener minimo 8 caracteres")
 	private String passwordConfirmation;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+
 }
