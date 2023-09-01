72f61a21be229d846a3fac5fd7af3196a4760c5f
Joaquín Corvalán <137232156+JoaquinCorv@users.noreply.github.com>
2023-09-01 13:28:17 -0300

    creacion del proyecto
    creamos el proyecto para iniciar nuestro proyecto

---
+++ CTUni/src/main/java/com/dojo/workspace/CTUni/CtUniApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.workspace.CTUni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtUniApplication.class, args);
+	}
+
+}
---
+++ CTUni/src/test/java/com/dojo/workspace/CTUni/CtUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.workspace.CTUni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
