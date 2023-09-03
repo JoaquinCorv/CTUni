1ba3af19bae9eddf95b55e1f58847d9076a511c0
Petrucci Tomas <124310933+PetrucciTomas@users.noreply.github.com>
2023-09-03 18:32:12 -0300

    mi primer cambio

--- CTUni/src/main/java/com/dojo/workspace/CTUni/controllers/MainController.java
+++ CTUni/src/main/java/com/dojo/workspace/CTUni/controllers/MainController.java
@@ -1,12 +1,13 @@
 package com.dojo.workspace.CTUni.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
-
-	
-	
+	@GetMapping("/")
+	public String main(Model viewModel) {
+		return "inicio.jsp";
+	}
 }
-
