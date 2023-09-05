8abd2d1c06e65432aa23336d7a29c153790f1a1a
Joaquín Corvalán <137232156+JoaquinCorv@users.noreply.github.com>
2023-09-05 20:21:47 -0300

    corregi el js

--- CTUni/src/main/resources/static/js/app.js
+++ CTUni/src/main/resources/static/js/app.js
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
 function menuDesplegable(){
 	const barraDeNavegacion = document.getElementById("categorias");
 	barraDeNavegacion.classList.toggle("show")
 }
-=======
->>>>>>> Stashed changes
