87f9bc83a457b983cec669f20e6a13e12a9a9bff
Julian Albornoz <129104628+julianliyo@users.noreply.github.com>
2023-09-13 01:17:37 -0300

    user

--- CTUni/src/main/java/com/dojo/workspace/CTUni/controllers/usercontroller.java
+++ CTUni/src/main/java/com/dojo/workspace/CTUni/controllers/usercontroller.java
@@ -1,5 +1,7 @@
 package com.dojo.workspace.CTUni.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,19 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.dojo.workspace.CTUni.models.Universidades;
 import com.dojo.workspace.CTUni.models.Usuario;
 import com.dojo.workspace.CTUni.models.logreg;
 import com.dojo.workspace.CTUni.services.Userservices;
-
+import com.dojo.workspace.CTUni.services.CTuniServices;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
 public class usercontroller {
-	
+	private final CTuniServices ctuniServices;
 	private final Userservices userServ;
-	public usercontroller(Userservices uSer) {
+	public usercontroller(Userservices uSer, CTuniServices ctuniservices) {
 		this.userServ = uSer;
+		this.ctuniServices = ctuniservices;
 
 	}
 	
@@ -27,6 +31,8 @@ public class usercontroller {
 	public String raiz(Model viewModel) {
 		viewModel.addAttribute("user", new Usuario());
 		viewModel.addAttribute("login", new logreg());
+		List<Universidades> universidades = ctuniServices.obtenerTodasLasUniversidades();
+		viewModel.addAttribute("universidades", universidades);
 		return "logreg.jsp";
 	}
 	
